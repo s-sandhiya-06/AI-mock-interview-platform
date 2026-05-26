f1d9583753204861af0383bc0a73696154cdb12d
Sandhiya S <ssandhiyasudhakar06@gmail.com>
2026-05-26 12:00:54 +0530

    Initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Interview from './pages/Interview'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/interview" element={<Interview />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
